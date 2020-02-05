fa1eec1282b2b0422297d24a3b2812c206d69d30
mricim <ericcasanova.m@gmail.com>
2020-02-05 20:59:59 +0100

    4

--- baseDeDatos/conectar.py
+++ baseDeDatos/conectar.py
@@ -1,6 +1,6 @@
 import psycopg2
 try:
     #conn = psycopg2.connect("dbname='m1' user='postgres' host='localhost' password='jupiter'")
-    conn = psycopg2.connect("dbname='m1' user='odoo1' host='localhost' password='odoo1'")
+    conn = psycopg2.connect("dbname='mydb' user='myuser' host='localhost' password='mypass'")
 except:
     print("No és possible connectar a la Base de Dades")
